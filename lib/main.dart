595b80c9c513d658ff6e656060b63f8e922c6afe
Abhishek Kannaujia <abhishekkannaujia96@gmail.com>
2023-07-22 14:35:24 +0530

    QUIZ APP

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,11 @@ class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Center(
-            child: Text('QUIZZLER'),
+            child: Text('QUIZZING'),
           ),
           backgroundColor: Colors.black26,
         ),
@@ -35,39 +36,58 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+  int totalCorrectAnswers = 0;
   void checkAnswer(bool userPickedAnswer) {
     bool correctAnswer = quizBrain.getCorrectAnswer();
-    setState(
-      () {
-        if (quizBrain.isFinished() == true) {
-          Alert(
-            context: context,
-            title: 'Finished!',
-            desc: 'You\'ve reached the end of the quiz.',
-          ).show();
-          quizBrain.reset();
-          scoreKeeper = [];
+    setState(() {
+      if (quizBrain.isFinished() == true) {
+        if (userPickedAnswer == correctAnswer) {
+          totalCorrectAnswers++; // Increment the count for correct answers.
+          scoreKeeper.add(
+            const Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+          );
         } else {
-          if (userPickedAnswer == correctAnswer) {
-            scoreKeeper.add(
-              const Icon(
-                Icons.check,
-                color: Colors.green,
-              ),
-            );
-          } else {
-            scoreKeeper.add(
-              const Icon(
-                Icons.close,
-                color: Colors.red,
-              ),
-            );
-          }
-          quizBrain.nextQuestion();
+          scoreKeeper.add(
+            const Icon(
+              Icons.close,
+              color: Colors.red,
+            ),
+          );
         }
-      },
-    );
+
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.\n\nTotal Correct Answers: $totalCorrectAnswers',
+        ).show();
+        quizBrain.reset();
+        scoreKeeper = [];
+        totalCorrectAnswers = 0; // Reset the count for the next quiz.
+      } else {
+        if (userPickedAnswer == correctAnswer) {
+          totalCorrectAnswers++; // Increment the count for correct answers.
+          scoreKeeper.add(
+            const Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+          );
+        } else {
+          scoreKeeper.add(
+            const Icon(
+              Icons.close,
+              color: Colors.red,
+            ),
+          );
+        }
+        quizBrain.nextQuestion();
+      }
+    });
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -91,40 +111,48 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        Expanded(
+        InkWell(
+          onTap: () {
+            checkAnswer(true);
+          },
           child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: const Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              height: 30,
+              decoration: BoxDecoration(color: Colors.green,),
+              child: const Center(
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
                 ),
               ),
-              onPressed: () {
-                checkAnswer(true);
-              },
+
             ),
           ),
         ),
-        Expanded(
+
+        InkWell(
+          onTap: () {
+            checkAnswer(false);
+          },
           child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: const Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              height: 30,
+              decoration: BoxDecoration(color: Colors.red,),
+              child: const Center(
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
                 ),
               ),
-              onPressed: () {
-                checkAnswer(false);
-              },
+
             ),
           ),
         ),
@@ -135,6 +163,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-// ,
-// ,
-//
--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -4,12 +4,10 @@ class QuizBrain {
   int _questionNumber = 0;
   final List<Question> _questionBank = [
     Question('Delhi is the capital of India', true), //object
-    Question('Kohli will hit a century in next match', false),
-    Question(
-        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        true),
-    Question('SRK is the biggest actor in the world', true),
-    Question('It is illegal to pee in the Ocean in Portugal.', true)
+    Question('Crow is the national bird of India', false),
+    Question('2+5 = 7 ', true),
+    Question('There are total 4 vowels', false),
+    Question('Earth revolves around the sun', true)
   ];
   void nextQuestion() {
     if (_questionNumber < _questionBank.length - 1) {
@@ -26,10 +24,11 @@ class QuizBrain {
   }
 
   bool isFinished() {
-    if (_questionNumber >= _questionBank.length - 1) {
+    if (_questionNumber >= _questionBank.length -1) {
       return true;
-    } else
+    } else {
       return false;
+    }
   }
 
   void reset() {
